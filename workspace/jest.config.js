cd6e9cf3261f6339c88085767385d0fd67492a8a
Juliano Pezzini <juliano.pezzini@philips.com>
2026-01-08 17:38:33 -0300

    chore: reset coverage thresholds to zero

--- workspace/jest.config.js
+++ workspace/jest.config.js
@@ -1,26 +1,26 @@
 export default {
   testEnvironment: 'jsdom',
-  
+
   // Setup files
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
-  
+
   // Module paths
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/resources/js/$1',
     '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
   },
-  
+
   // Transform files
   transform: {
     '^.+\\.(js|jsx)$': ['babel-jest', { configFile: './babel.config.cjs' }],
   },
-  
+
   // Test match patterns
   testMatch: [
     '<rootDir>/resources/js/**/__tests__/**/*.{js,jsx}',
     '<rootDir>/resources/js/**/*.{spec,test}.{js,jsx}',
   ],
-  
+
   // Coverage configuration
   collectCoverageFrom: [
     'resources/js/**/*.{js,jsx}',
@@ -28,22 +28,22 @@ export default {
     '!resources/js/**/__tests__/**',
     '!resources/js/**/index.js',
   ],
-  
+
   coverageThreshold: {
     global: {
-      branches: 50,
-      functions: 50,
-      lines: 50,
-      statements: 50,
+      branches: 0,
+      functions: 0,
+      lines: 0,
+      statements: 0,
     },
   },
-  
+
   // Ignore patterns
   testPathIgnorePatterns: [
     '/node_modules/',
     '/vendor/',
     '/storage/',
   ],
-  
+
   moduleFileExtensions: ['js', 'jsx', 'json'],
 };
