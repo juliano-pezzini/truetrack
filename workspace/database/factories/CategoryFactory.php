830ce6d3cfb8ed8dde8226ef69e3ebb380bae16c
Juliano Pezzini <juliano.pezzini@philips.com>
2026-01-10 19:54:34 -0300

    style: fix code style in CategoryFactory (Laravel Pint)

--- workspace/database/factories/CategoryFactory.php
+++ workspace/database/factories/CategoryFactory.php
@@ -18,6 +18,7 @@ class CategoryFactory extends Factory
      * Define the model's default state.
      *
      * @return array<string, mixed>
+     *
      * @phpstan-return array<string, mixed>
      */
     public function definition(): array
