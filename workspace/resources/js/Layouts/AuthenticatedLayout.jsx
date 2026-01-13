f48c52869ead6552cfd26c7bc8e5d81409c80d0b
Juliano Pezzini <juliano.pezzini@philips.com>
2026-01-13 14:24:25 -0300

    feat: Add settings navigation with categories and tags access (#16)

--- workspace/resources/js/Layouts/AuthenticatedLayout.jsx
+++ workspace/resources/js/Layouts/AuthenticatedLayout.jsx
@@ -52,6 +52,54 @@ export default function AuthenticatedLayout({ header, children }) {
                         </div>
 
                         <div className="hidden sm:ms-6 sm:flex sm:items-center">
+                            {/* Settings Dropdown */}
+                            <div className="relative ms-3">
+                                <Dropdown>
+                                    <Dropdown.Trigger>
+                                        <span className="inline-flex rounded-md">
+                                            <button
+                                                type="button"
+                                                className="inline-flex items-center rounded-md border border-transparent bg-white p-2 text-sm font-medium leading-4 text-gray-500 transition duration-150 ease-in-out hover:text-gray-700 focus:outline-none"
+                                            >
+                                                <svg
+                                                    className="h-5 w-5"
+                                                    xmlns="http://www.w3.org/2000/svg"
+                                                    fill="none"
+                                                    viewBox="0 0 24 24"
+                                                    strokeWidth="1.5"
+                                                    stroke="currentColor"
+                                                >
+                                                    <path
+                                                        strokeLinecap="round"
+                                                        strokeLinejoin="round"
+                                                        d="M9.594 3.94c.09-.542.56-.94 1.11-.94h2.593c.55 0 1.02.398 1.11.94l.213 1.281c.063.374.313.686.645.87.074.04.147.083.22.127.324.196.72.257 1.075.124l1.217-.456a1.125 1.125 0 011.37.49l1.296 2.247a1.125 1.125 0 01-.26 1.431l-1.003.827c-.293.24-.438.613-.431.992a6.759 6.759 0 010 .255c-.007.378.138.75.43.99l1.005.828c.424.35.534.954.26 1.43l-1.298 2.247a1.125 1.125 0 01-1.369.491l-1.217-.456c-.355-.133-.75-.072-1.076.124a6.57 6.57 0 01-.22.128c-.331.183-.581.495-.644.869l-.213 1.28c-.09.543-.56.941-1.11.941h-2.594c-.55 0-1.02-.398-1.11-.94l-.213-1.281c-.062-.374-.312-.686-.644-.87a6.52 6.52 0 01-.22-.127c-.325-.196-.72-.257-1.076-.124l-1.217.456a1.125 1.125 0 01-1.369-.49l-1.297-2.247a1.125 1.125 0 01.26-1.431l1.004-.827c.292-.24.437-.613.43-.992a6.932 6.932 0 010-.255c.007-.378-.138-.75-.43-.99l-1.004-.828a1.125 1.125 0 01-.26-1.43l1.297-2.247a1.125 1.125 0 011.37-.491l1.216.456c.356.133.751.072 1.076-.124.072-.044.146-.087.22-.128.332-.183.582-.495.644-.869l.214-1.281z"
+                                                    />
+                                                    <path
+                                                        strokeLinecap="round"
+                                                        strokeLinejoin="round"
+                                                        d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
+                                                    />
+                                                </svg>
+                                            </button>
+                                        </span>
+                                    </Dropdown.Trigger>
+
+                                    <Dropdown.Content>
+                                        <Dropdown.Link
+                                            href={route('categories.index')}
+                                        >
+                                            Categories
+                                        </Dropdown.Link>
+                                        <Dropdown.Link
+                                            href={route('tags.index')}
+                                        >
+                                            Tags
+                                        </Dropdown.Link>
+                                    </Dropdown.Content>
+                                </Dropdown>
+                            </div>
+
+                            {/* Profile Dropdown */}
                             <div className="relative ms-3">
                                 <Dropdown>
                                     <Dropdown.Trigger>
@@ -170,6 +218,28 @@ export default function AuthenticatedLayout({ header, children }) {
                         >
                             Reconciliations
                         </ResponsiveNavLink>
+                    </div>
+
+                    <div className="border-t border-gray-200 pb-3 pt-4">
+                        <div className="px-4 pb-1">
+                            <div className="text-sm font-medium text-gray-500">
+                                Settings
+                            </div>
+                        </div>
+                        <div className="space-y-1">
+                            <ResponsiveNavLink
+                                href={route('categories.index')}
+                                active={route().current('categories.*')}
+                            >
+                                Categories
+                            </ResponsiveNavLink>
+                            <ResponsiveNavLink
+                                href={route('tags.index')}
+                                active={route().current('tags.*')}
+                            >
+                                Tags
+                            </ResponsiveNavLink>
+                        </div>
                     </div>
 
                     <div className="border-t border-gray-200 pb-1 pt-4">
