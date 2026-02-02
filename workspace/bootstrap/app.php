1efd8300c21227cd7061e61127e22bc1bcf857ac
Juliano Pezzini <juliano.pezzini@philips.com>
2026-02-02 19:29:58 -0300

    chore: revert helpers.php

--- workspace/bootstrap/app.php
+++ workspace/bootstrap/app.php
@@ -4,9 +4,6 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
-// Load helper functions
-require_once __DIR__.'/../app/helpers.php';
-
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
